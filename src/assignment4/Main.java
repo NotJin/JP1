0cbbfe2165e4b93408b3ee6cdac38f2f2dd303f8
Đồng Vũ <Dongvu7173@gmail.com>
2022-10-21 18:17:42 +0700

    Assignment5

---
+++ src/assignment4/Main.java
@@ -0,0 +1,10 @@
+package assignment4;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+
+    }
+}
---
+++ src/session4/MaleInterface.java
@@ -0,0 +1,6 @@
+package session4;
+
+public interface MaleInterface {
+    void playGame();
+    void playFootBall();//void abstract playFootball();
+}
